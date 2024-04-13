58298d903cccd419923cc38f30e40998b3e85b71
Godfred Kogkane <123625779+godfredkogkane@users.noreply.github.com>
2024-04-13 01:05:10 +0000

    made changes to connection

--- watertracker/settings/connection.php
+++ watertracker/settings/connection.php
@@ -1,14 +1,28 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'water_tracker');
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASSWORD', '');
+// define('DB_NAME', 'water_tracker');
 
 
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+// $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+
+
+define('DB_HOST', 'us-cluster-east-01.k8s.cleardb.net');
+define('DB_USER', 'bd80e3f607d2ea');
+define('DB_PASSWORD', 'ade1214f'); 
+define('DB_NAME', 'heroku_f7dfd3145aba5cc');
+
+
+$con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+} 
+
 ?>
